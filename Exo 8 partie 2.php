570055887d5cb6cccaef4ac46b4926bef9d08895
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-09 15:47:21 +0200

    Rajout des balises P

---
+++ Exo 8 partie 2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 8 : Les conditions</title>
+</head>
+<body>
+    <h1>Exo 8 : Les conditions</h1>
+    <h4>Traduire ce code avec des if et des else : </h4>
+    <p>
+    <?php
+    $isOK = true;
+    if ($isOK === false)
+      {
+        echo 'C\'est OK';
+      }
+      elseif ($isOK === true)
+      {
+        echo 'C\'est pas bon';
+      }
+    ?>
+    </p>
+</body>
+</html>
